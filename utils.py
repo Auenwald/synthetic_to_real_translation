d2eee7907069ef8de53d73b32ac17805cdad03f8
auenwald94 <marcbaetje@web.de>
2025-08-01 10:17:29 +0000

    smal changes

--- utils.py
+++ utils.py
@@ -17,7 +17,7 @@ def get_augmentation(dataset_name, split):
                 # A.HorizontalFlip(p=0.5),
                 # A.Blur(blur_limit=(3, 7), p=0.5),
                 # A.RandomBrightnessContrast(p=0.2),
-                # A.RandomBrightnessContrast(brightness_limit=1, contrast_limit=1, p=0.5),
+                A.RandomBrightnessContrast(brightness_limit=1, contrast_limit=1, p=0.5),
                 # A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1, p=0.5),
                 # A.RandomRotate90(p=0.5),
                 A.Resize(380, 640),
