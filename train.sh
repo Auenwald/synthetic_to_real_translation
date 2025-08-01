7e5303de111a0218c4a4f76f79173cd69253a90d
auenwald94 <marcbaetje@web.de>
2025-08-01 09:28:43 +0000

    small changes

--- train.sh
+++ train.sh
@@ -27,7 +27,24 @@ python trainv3.py \
   --optimizer SGD \
   --epochs 70 \
   --use_logging True \
-  --log_file ./logs_final/synthia_to_cityscapes_segformer_b5_SGD_lr_5e03_decay_0995.txt \
+  --log_file ./logs_final/synthia_to_cityscapes_deeplab_b5_SGD_lr_5e03_decay_0995.txt \
+  --skip_val_source false \
+  --decay_factor 0.995 \
+  --weight_averaging True \
+  --train_print_steps 50
+  ############################# 
+
+########### SYNTHIASTYLE - DEEPLAB-V3 - SGD #########
+python trainv3.py \
+  --source_path ./synthiastyle \
+  --target_path ./cityscapes \
+  --batch_size 4 \
+  --model_name deeplab \
+  --lr 5.0e-3 \
+  --optimizer SGD \
+  --epochs 70 \
+  --use_logging True \
+  --log_file ./logs_final/synthiastyle_to_cityscapes_deeplab_b5_SGD_lr_5e03_decay_0995.txt \
   --skip_val_source false \
   --decay_factor 0.995 \
   --weight_averaging True \
