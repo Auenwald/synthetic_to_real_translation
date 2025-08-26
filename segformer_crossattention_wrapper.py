fixed miou calculation

segformer_crossattention_wrapper.py
@@ -36,9 +36,9 @@ class MultiHeadCrossAttention(nn.Module):
 class FeatureDropout(nn.Module):
     def __init__(self, drop_prob=0.3, mode="branch"):
         """
-        drop_prob: Wahrscheinlichkeit fürs Droppen
-        mode: "branch" -> RGB oder Hybrid-Branch komplett droppen
-              "channel" -> Kanäle im Hybrid-Branch maskieren
+        drop_prob: probability for dropping one of the branches
+        mode: "branch" -> drop rgb or hybrid branch entirely
+              "channel" -> mask channels within the hybrid branch
         """
         super().__init__()
         self.drop_prob = drop_prob
@@ -50,14 +50,13 @@ class FeatureDropout(nn.Module):
 
         if self.mode == "branch":
             if torch.rand(1).item() < self.drop_prob:
-                # wähle zufällig, welche Branch gedroppt wird
+                # drop one branch
                 if torch.rand(1).item() < 0.5:
                     feat_rgb = torch.zeros_like(feat_rgb)
                 else:
                     feat_hybrid = torch.zeros_like(feat_hybrid)
 
         elif self.mode == "channel":
-            # Kanäle im Hybrid droppen
             mask = (torch.rand(feat_hybrid.size(1), device=feat_hybrid.device) > self.drop_prob).float()
             feat_hybrid = feat_hybrid * mask.view(1, -1, 1, 1)
 
@@ -69,10 +68,10 @@ class SegformerCrossAttentionWrapper(nn.Module):
                  cross_attn_dims=[64, 128, 256, 384], 
                  downsample_factor=0.5,
                    num_classes=16,
-                   hybrid_out_ch=4):
+                   hybrid_out_ch=1):
         super().__init__()
 
-        # RGB-Branch
+        # rgb branch
         base_model_rgb = SegformerForSemanticSegmentation.from_pretrained(segformer_name, num_labels=num_classes)
         self.encoder_rgb = base_model_rgb.segformer.encoder
         self.decoder = base_model_rgb.decode_head
@@ -80,16 +79,16 @@ class SegformerCrossAttentionWrapper(nn.Module):
         config = self.encoder_rgb.config
         self.encoder_feat_hybrid = SegformerModel(config).encoder
 
-        # Patch embedding auf 1 Channel anpassen
+        # adjust input embedding dim to number of modalities + channels
         self.encoder_feat_hybrid.patch_embeddings[0].proj = nn.Conv2d(
-            in_channels=1,
+            in_channels=hybrid_out_ch,
             out_channels=self.encoder_feat_hybrid.config.hidden_sizes[0],
             kernel_size=7,
             stride=4,
             padding=3
         )
 
-        self.feature_dropout = FeatureDropout(drop_prob=0.3, mode="branch")
+        # self.feature_dropout = FeatureDropout(drop_prob=0.3, mode="branch")
 
         self.cross_attn_layers = nn.ModuleList([
             MultiHeadCrossAttention(in_dim=c, attn_dim=d) 
@@ -121,18 +120,18 @@ class SegformerCrossAttentionWrapper(nn.Module):
         return: [B, 1, H, W] log-Magnitude
         """
 
-        # FFT pro Kanal
+        # fft per channel
         fft = torch.fft.fft2(image_rgb)                   
         fft_shift = torch.fft.fftshift(fft, dim=(-2, -1))
         magnitude = torch.abs(fft_shift)
         
-        # Mittelwert über RGB-Kanäle → 1 Channel
+        # mean over rgb channel -> 1 channel
         magnitude_1ch = magnitude.mean(dim=1, keepdim=True)
         
         # log-Skalierung
         log_mag = torch.log1p(magnitude_1ch)
         
-        # Normalisierung
+        # normalization
         log_mag = (log_mag - log_mag.mean(dim=[2,3], keepdim=True)) / (log_mag.std(dim=[2,3], keepdim=True) + 1e-6)
     
         return log_mag  # [B, 1, H, W]
@@ -142,13 +141,13 @@ class SegformerCrossAttentionWrapper(nn.Module):
         image_rgb: [B, C, H, W]
         return: [B, 1, H, W] DCT-Magnitude
         """
-        # DCT pro Kanal
+        # dct per channel
         dct_out = dct_2d(image_rgb)
 
-        # Mittelwert über Kanäle → 1 Channel
+        # mean over channels → 1 channel
         dct_1ch = dct_out.mean(dim=1, keepdim=True)
 
-        # Normalisierung
+        # normalization step
         dct_1ch = (dct_1ch - dct_1ch.mean(dim=[2,3], keepdim=True)) / (dct_1ch.std(dim=[2,3], keepdim=True) + 1e-6)
     
         return dct_1ch  # [B, 1, H, W]
@@ -158,7 +157,7 @@ class SegformerCrossAttentionWrapper(nn.Module):
         fft_1ch = self.fft_magnitude_1ch(image_rgb)  # [B, 1, H, W]
         dct_1ch = self.dct_map_1ch(image_rgb)        # [B, 1, H, W]
         
-        # Stack → 2 Kanäle
+        # stack → 2 Kanäle
         combined = torch.cat([fft_1ch, dct_1ch], dim=1)  # [B, 2, H, W]
         return combined
 
@@ -167,7 +166,7 @@ class SegformerCrossAttentionWrapper(nn.Module):
         dct_1ch = self.dct_map_1ch(image_rgb)        # [B, 1, H, W]
         edge_1ch = utils.multiscale_scharr_edges(image_rgb)
 
-        # Stack → 3 Kanäle
+        # stack → 3 Kanäle
         combined = torch.cat([fft_1ch, dct_1ch, edge_1ch], dim=1)  # [B, 2, H, W]
         return combined
 
@@ -208,7 +207,7 @@ class SegformerCrossAttentionWrapper(nn.Module):
             B, C, H, W = rgb_hidden_states[i].shape
 
             # feature dropout
-            rgb_hidden_states[i], feat_hybrid_hidden_states[i] = self.feature_dropout(rgb_hidden_states[i], feat_hybrid_hidden_states[i])
+            # rgb_hidden_states[i], feat_hybrid_hidden_states[i] = self.feature_dropout(rgb_hidden_states[i], feat_hybrid_hidden_states[i])
 
             # # downsampling
             rgb_small = F.interpolate(rgb_hidden_states[i], scale_factor=self.downsample_factor, mode='bilinear', align_corners=False)

train_crossattention_lr_scheduling_segformer.py
@@ -246,7 +246,7 @@ def validate(val_loader, model, DEVICE, applied_ema, dataset_name, epoch, max_ep
 
         with torch.no_grad():
              output = model_utils.get_logits(model, data)
-             output = torch.nn.functional.interpolate(output, size=utils.get_image_size(dataset_name), mode='bilinear')
+             output = torch.nn.functional.interpolate(output, size=utils.get_image_size(dataset_name), mode='bilinear', align_corners=False)
 
         preds = torch.argmax(output, dim=1)          
         mean_iou = jaccard_index(task='multiclass', ignore_index=255, num_classes=num_classes, preds=preds, target=targets) * 100

train_crossattention_segformer.py
@@ -167,7 +167,7 @@ def train(train_loader, model, optim, loss_fn, DEVICE, ema, PRINT_INTERVAL, AVER
             continue
 
         h, w = data.shape[2], data.shape[3]
-        logits = torch.nn.functional.interpolate(logits, size=(h, w), mode='bilinear')
+        logits = torch.nn.functional.interpolate(logits, size=(h, w), mode='bilinear', align_corners=False)
     
         loss = loss_fn(logits, targets)
 

trainv4_crossattention_segformer.py
@@ -12,6 +12,9 @@ from torch_ema import ExponentialMovingAverage
 import pytorch_warmup as  warmup
 import model_utils
 from segformer_crossattention_wrapper import *
+import os
+import json
+from losses import CombinedLoss
 
 # import torchmetrics
 from torchmetrics.functional import jaccard_index
@@ -94,7 +97,9 @@ def main():
     # model = SegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b1-finetuned-ade-512-512", ignore_mismatched_sizes=True, num_labels=num_classes)
     
 
-    model = model_utils.get_model_by_name(MODEL_NAME, num_classes)
+    # model = model_utils.get_model_by_name(MODEL_NAME, num_classes)
+    model = SegformerCrossAttentionWrapper(segformer_name='nvidia/mit-b5')
+
     model = model.to(DEVICE)
    
     if args.optimizer.lower() == 'sgd': 
@@ -109,14 +114,10 @@ def main():
     else:
         ema = None
 
-    loss_fn = torch.nn.CrossEntropyLoss(ignore_index=255)
+    # loss_fn = torch.nn.CrossEntropyLoss(ignore_index=255)
+    loss_fn = CombinedLoss(ce_weight=0.5, dice_weight=0.5, ignore_index=255)
 
     for epoch in range(1 + epoch_modifier, EPOCHS + 1 + epoch_modifier):
-         
-        scores[TARGET_DATASET_NAME][epoch] = []
-        scores[TARGET_DATASET_NAME + "-ema"][epoch] = []
-        scores[SOURCE_DATASET_NAME][epoch] = []
-        scores[SOURCE_DATASET_NAME + "-ema"][epoch] = []
          
         train(source_train_data_loader, model, optim, loss_fn, DEVICE, ema, PRINT_INTERVAL, AVERAGING_INTERVAL, SOURCE_DATASET_NAME)
 
@@ -126,15 +127,15 @@ def main():
         if WEIGHT_AVERAGING:
             with ema.average_parameters():
                 if not SKIP_VAL_SOURCE:
-                    validate(source_val_data_loader, model, DEVICE, True, f'{SOURCE_DATASET_NAME}', epoch, EPOCHS)
-                validate(target_val_data_loader, model, DEVICE, True, f'{TARGET_DATASET_NAME}', epoch, EPOCHS)
+                    validate(source_val_data_loader, model, DEVICE, LOG_PATH, True, f'{SOURCE_DATASET_NAME}', epoch, EPOCHS)
+                validate(target_val_data_loader, model, DEVICE, LOG_PATH, True, f'{TARGET_DATASET_NAME}', epoch, EPOCHS)
         
         if not SKIP_VAL_SOURCE:
-            validate(source_val_data_loader, model, DEVICE, False, f'{SOURCE_DATASET_NAME}', epoch, EPOCHS)
-        validate(target_val_data_loader, model, DEVICE, False, f'{TARGET_DATASET_NAME}', epoch, EPOCHS)
+            validate(source_val_data_loader, model, DEVICE, LOG_PATH, False, f'{SOURCE_DATASET_NAME}', epoch, EPOCHS)
+        validate(target_val_data_loader, model, DEVICE, LOG_PATH, False, f'{TARGET_DATASET_NAME}', epoch, EPOCHS)
         
 
-        write_scores_to_log_file(LOG_PATH, epoch, WEIGHT_AVERAGING, SOURCE_DATASET_NAME, TARGET_DATASET_NAME)
+        # write_scores_to_log_file(LOG_PATH, epoch, WEIGHT_AVERAGING, SOURCE_DATASET_NAME, TARGET_DATASET_NAME)
     
     # TODO: save model if necessary (meanIOU > bestMeanIOU)
 
@@ -176,45 +177,82 @@ def train(train_loader, model, optim, loss_fn, DEVICE, ema, PRINT_INTERVAL, AVER
             print(f'[train-{SOURCE_DATASET_NAME}] Progress: {i}/{len(train_loader)}, mean-IoU: {mean_iou:.2f}, lr: {optim.param_groups[0]["lr"]}')
  
     
-def validate(val_loader, model, DEVICE, applied_ema, dataset_name, epoch, max_epochs):
-     model.eval()
-     suffix = "-ema" if applied_ema else ""
-     dataset_key = dataset_name + suffix
+def validate(val_loader, model, DEVICE, LOG_PATH, applied_ema, dataset_name, epoch, max_epochs):
+    model.eval()
+    suffix = "-ema" if applied_ema else ""
+    dataset_key = dataset_name + suffix
 
-     for idx, (data, targets) in enumerate(val_loader):
-        
+    all_preds = []
+    all_targets = []
+    
+
+    for idx, (data, targets) in enumerate(val_loader):
+    
         if data is None or targets is None:
             continue
 
         data, targets = data.to(DEVICE), targets.to(DEVICE).long()
 
         with torch.no_grad():
-             output = model_utils.get_logits(model, data)
-             output = torch.nn.functional.interpolate(output, size=utils.get_image_size(dataset_name), mode='bilinear', align_corners=False)
+                output = model_utils.get_logits(model, data)
+                output = torch.nn.functional.interpolate(output, size=utils.get_image_size(dataset_name), mode='bilinear', align_corners=False)
 
-        preds = torch.argmax(output, dim=1)          
-        mean_iou = jaccard_index(task='multiclass', ignore_index=255, num_classes=num_classes, preds=preds, target=targets) * 100
-
-        # write into scores
-        print(f'[val-{dataset_name}{suffix}] - Epoch: {epoch}/{max_epochs} '
-              f'Progress: {idx}/{len(val_loader)}, mean-IoU: {mean_iou:.2f}')
+        preds = torch.argmax(output, dim=1)     
+        all_preds.append(preds)
+        all_targets.append(targets)    
         
-        scores[dataset_key][epoch].append(round(mean_iou.item(), 3))
+
+        if idx % 10 == 0:
+            print(f'[val-{dataset_name}{suffix}] - Epoch: {epoch}/{max_epochs} '
+                f'Progress: {idx + 1}/{len(val_loader)}')
+            
+        
+    all_preds = torch.cat(all_preds, dim=0)
+    all_targets = torch.cat(all_targets, dim=0)   
+
+    miou, per_class_miou = utils.compute_mIoU_and_per_class(all_preds, all_targets, num_classes=num_classes) 
+    
 
 
+    LOG_JSON_PATH = LOG_PATH
+    scores = {}
 
-def write_scores_to_log_file(LOG_PATH, epoch, APPLY_AVERAGING, SOURCE_DATASET_NAME, TARGET_DATASET_NAME):
+    if LOG_JSON_PATH and os.path.exists(LOG_JSON_PATH) and os.path.getsize(LOG_JSON_PATH) > 0:
+        try:
+            with open(LOG_JSON_PATH, 'r') as f:
+                    scores = json.load(f)
+        except json.JSONDecodeError:
+            print(f"Warnung: {LOG_JSON_PATH} ist leer oder ungültig. Starte mit leerem Dict.")
 
-    datasets_to_log = [TARGET_DATASET_NAME, SOURCE_DATASET_NAME]
+    if dataset_key not in scores:
+        scores[dataset_key] = {}
 
-    if APPLY_AVERAGING:
-        datasets_to_log += [f'{TARGET_DATASET_NAME}-ema', f'{SOURCE_DATASET_NAME}-ema']
+    # Epoch-Daten immer setzen
+    scores[dataset_key][str(epoch)] = {
+        "mean_iou": round(miou * 100, 3),
+        "per_class_iou": {str(k): round(v*100, 3) for k, v in per_class_miou.items()}
+    }
 
-    with open(LOG_PATH, 'a') as f:
-        for dataset_key in datasets_to_log:
-            scores_for_epoch = scores[dataset_key][epoch]
-            line = f"{dataset_key} {epoch} " + " ".join(str(iou) for iou in scores_for_epoch) + "\n"
-            f.write(line)
+     # JSON zurückschreiben
+    if LOG_JSON_PATH:
+        with open(LOG_JSON_PATH, 'w') as f:
+            json.dump(scores, f, indent=4)
+
+    print(f'[val-{dataset_name}{suffix}] - Epoch: {epoch}/{max_epochs} - mean-IoU: {miou*100:.2f}')
+
+
+# def write_scores_to_log_file(LOG_PATH, epoch, APPLY_AVERAGING, SOURCE_DATASET_NAME, TARGET_DATASET_NAME):
+
+#     datasets_to_log = [TARGET_DATASET_NAME, SOURCE_DATASET_NAME]
+
+#     if APPLY_AVERAGING:
+#         datasets_to_log += [f'{TARGET_DATASET_NAME}-ema', f'{SOURCE_DATASET_NAME}-ema']
+
+#     with open(LOG_PATH, 'a') as f:
+#         for dataset_key in datasets_to_log:
+#             scores_for_epoch = scores[dataset_key][epoch]
+#             line = f"{dataset_key} {epoch} " + " ".join(str(iou) for iou in scores_for_epoch) + "\n"
+#             f.write(line)
     
 if __name__ == '__main__':
     main()

utils.py
@@ -14,6 +14,32 @@ import torch.nn.functional as F
 
 
 
+
+def compute_mIoU_and_per_class(preds, labels, num_classes):
+    preds = preds.view(-1)
+    labels = labels.view(-1)
+    
+    mask = (labels >= 0) & (labels < num_classes)
+    preds = preds[mask]
+    labels = labels[mask]
+
+    conf_matrix = torch.bincount(
+        num_classes * labels + preds,
+        minlength=num_classes**2
+    ).reshape(num_classes, num_classes).float()
+
+    TP = conf_matrix.diag()
+    FP = conf_matrix.sum(dim=0) - TP
+    FN = conf_matrix.sum(dim=1) - TP
+
+    per_class_IoU = TP / (TP + FP + FN + 1e-6)
+    per_class_dict = {int(c): float(per_class_IoU[c].item())
+                      for c in range(num_classes) if conf_matrix.sum(dim=1)[c] > 0}
+
+    mean_iou = sum(per_class_dict.values()) / len(per_class_dict)
+    return mean_iou, per_class_dict
+
+
 def sobel_edges(images):
     # images: [B, 3, H, W] -> Graustufen + Sobel
     gray = images.mean(dim=1, keepdim=True)  # [B,1,H,W]
@@ -177,30 +203,32 @@ def multiscale_scharr_edges(images, sigmas=(0.5, 1.0, 2.0)):
 
 def get_augmentation(dataset_name, split):
     dataset_name = dataset_name.lower()
-    
-    additional_targets = {'mask': 'mask'}  # Masken immer korrekt behandeln
 
     if 'synthia' in dataset_name:
         if split == 'train':
             return A.Compose([
-                A.RandomCrop(width=640, height=380),
+                # A.RandomCrop(width=640, height=380),
                 A.HorizontalFlip(p=0.5),
+                A.RandomResizedCrop(size=(380, 640), scale=(0.8, 1.0), ratio=(0.75, 1.33)),
                 A.OneOf([
-                    A.ColorJitter(brightness=0.3, contrast=0.2, saturation=0.2, hue=0.05),
-                    A.RandomBrightnessContrast(brightness_limit=(-0.15, 0.3), contrast_limit=0.3)
+                    A.ColorJitter(brightness=0.3, contrast=0.2, saturation=0.3, hue=0.05, p=0.5),
+                    A.RandomBrightnessContrast(
+                        brightness_limit=(-0.2, 0.2), 
+                        contrast_limit=(-0.2, 0.2),
+                        p=0.5),
                 ], p=0.7),
-                A.GaussianBlur(blur_limit=(3, 5), p=0.2),
+                A.GaussianBlur(blur_limit=(3, 5), sigma_limit=(0.1, 2.0), p=0.2),
                 A.Normalize(mean=(0.485, 0.456, 0.406),
                             std=(0.229, 0.224, 0.225)),
                 ToTensorV2(),
-            ], additional_targets=additional_targets)
+            ])
         else:
             return A.Compose([
                 A.Resize(380, 640),
                 A.Normalize(mean=(0.485, 0.456, 0.406),
                             std=(0.229, 0.224, 0.225)),
                 ToTensorV2(),
-            ], additional_targets=additional_targets)
+            ])
 
     elif 'gta5' in dataset_name:
         if split == 'train':
@@ -210,14 +238,14 @@ def get_augmentation(dataset_name, split):
                 A.Normalize(mean=(0.485, 0.456, 0.406),
                             std=(0.229, 0.224, 0.225)),
                 ToTensorV2(),
-            ], additional_targets=additional_targets)
+            ])
         else:
             return A.Compose([
                 A.Resize(512, 1024),
                 A.Normalize(mean=(0.485, 0.456, 0.406),
                             std=(0.229, 0.224, 0.225)),
                 ToTensorV2(),
-            ], additional_targets=additional_targets)
+            ])
 
     elif 'cityscapes' in dataset_name:
         return A.Compose([
@@ -225,7 +253,7 @@ def get_augmentation(dataset_name, split):
             A.Normalize(mean=(0.485, 0.456, 0.406),
                         std=(0.229, 0.224, 0.225)),
             ToTensorV2(),
-        ], additional_targets=additional_targets)
+        ])
 
     elif 'bdd' in dataset_name:
         return A.Compose([
@@ -233,7 +261,7 @@ def get_augmentation(dataset_name, split):
             A.Normalize(mean=(0.485, 0.456, 0.406),
                         std=(0.229, 0.224, 0.225)),
             ToTensorV2(),
-        ], additional_targets=additional_targets)
+        ])
 
     else:
         # Fallback: Resize + Normalize
@@ -242,7 +270,7 @@ def get_augmentation(dataset_name, split):
             A.Normalize(mean=(0.485, 0.456, 0.406),
                         std=(0.229, 0.224, 0.225)),
             ToTensorV2(),
-        ], additional_targets=additional_targets)
+        ])
 
 
 def get_dataloader_from_dataset(path, dataset_name, split, batch_size, shuffle, use_synthia_shapes=False):